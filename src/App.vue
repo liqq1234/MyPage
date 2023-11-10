d77287d5393f1a35bb9d450a49e41051a18323d1
李庆全 <13580877+LIQQ1234@user.noreply.gitee.com>
2023-11-10 23:48:05 +0800

    初始化网站 修改标题和网站

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" style="width: 200px; height: auto;">    <HelloWorld msg="Welcome to LiQQ'music"/>
   </div>
 </template>
 
